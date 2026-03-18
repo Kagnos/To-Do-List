c85e11294914f5f823360f13897b51c1443d6c72
Kagnos <kagcodes@gmail.com>
2026-03-18 15:16:36 -0500

    Create createTask factory and TaskList array

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("tasks script running!");
-});
+const taskList = [];
+
+function createTask(title, description, dueDate, priority, completed) {
+    return { title, description, dueDate, priority, completed };
+};
+
+taskList.push(createTask("Exercise", "Run a mile", "03/18/26", "Medium", false));
