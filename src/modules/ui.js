f16aaa5fa7c70561677d67eb821c4406c00fd8bc
Kagnos <kagcodes@gmail.com>
2026-03-19 08:45:10 -0500

    Create, style, add event listeners for task dialog

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("ui script running!");
+const allButtons = document.querySelectorAll("button");
+
+const taskDialog = document.querySelector("#task-dialog");
+const taskDialogForm = document.querySelector("#task-dialog-form");
+
+allButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        switch(button.id) {
+            case "new-task-sidebar-button":
+                return taskDialog.showModal();
+            case "task-dialog-cancel-button":
+                taskDialogForm.reset();
+                return taskDialog.close();
+        };
+    });
 });
